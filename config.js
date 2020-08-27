acf7cfa611d37cb738ec5d91254f551421afefde
audi2654 <36697715+audi2654@users.noreply.github.com>
2020-08-27 18:35:01 +0530

    Added config.js file
    Added both a config.js file with js folder & only config.js file, whichever works

---
+++ config.js
@@ -0,0 +1,9 @@
+window._config = {
+
+    api: { 
+
+        invokeUrl: 'https://l04i8gf02e.execute-api.us-east-1.amazonaws.com/amplify-app-stage-1/contact-form-resource' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
+};
